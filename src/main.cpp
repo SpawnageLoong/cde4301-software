e9aeb82f3f6bce2d34a296545b34c0639a9ebcea
Richard Loong <richardloongcj@gmail.com>
2024-10-18 20:06:44 +0800

    Remove libcsp, re-add libcsp headers only

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,9 @@
 //
 //**************************************************************************
 
-#include <Arduino.h>
 #include <FreeRTOS_SAMD21.h>
+#include <csp/csp.h>
+#include <csp/interfaces/csp_if_can.h>
 
 //**************************************************************************
 // Type Defines and Constants
